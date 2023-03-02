437a15a31586994f04597490300142ce21f03ef9
Aghas2408 <aghasi.seyranyan@gmail.com>
2023-03-02 19:01:06 +0400

    global variables

--- contracts/Variables.sol
+++ contracts/Variables.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.9;
 contract Variables {
     uint256 public myUint = 123;
 
+    // view can read from state and global variables in blockchain
+    function globalVars() external view returns (address, uint, uint) {
+        address sender = msg.sender;
+        uint timestamp = block.timestamp;
+        uint blockNum = block.number;
+        return (sender, timestamp, blockNum);
+    }
+
     function foo() external {
         uint256 notStateVariable = 456;
 
